e7820147b0696962706cfc438701bfdcb0f5b158
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2023-02-27 21:11:35 +0200

    Finish UI of `DownloadFragment`

--- app/src/main/java/com/abdallah_abdelazim/loadapp/ui/download/DownloadFragment.kt
+++ app/src/main/java/com/abdallah_abdelazim/loadapp/ui/download/DownloadFragment.kt
@@ -5,35 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.abdallah_abdelazim.loadapp.databinding.FragmentDownloadBinding
 
 class DownloadFragment : Fragment() {
 
     private var _binding: FragmentDownloadBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentDownloadBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(
-                DownloadFragmentDirections.actionDownloadFragmentToDownloadDetailsFragment()
-            )
-        }
+//        binding.buttonFirst.setOnClickListener {
+//            findNavController().navigate(
+//                DownloadFragmentDirections.actionDownloadFragmentToDownloadDetailsFragment()
+//            )
+//        }
     }
 
     override fun onDestroyView() {
